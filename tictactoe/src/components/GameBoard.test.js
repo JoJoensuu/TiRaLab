28dd6df98996755cb749a2791f7c4ce304d378ca
Joni Joensuu <joni.joens@gmail.com>
2023-11-19 16:02:08 +0200

    more test coverage

--- tictactoe/src/components/GameBoard.test.js
+++ tictactoe/src/components/GameBoard.test.js
@@ -3,27 +3,22 @@ import '@testing-library/jest-dom';
 import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import GameBoard from './GameBoard';
 import * as gameHeuristics from '../logic/GameHeuristics';
-
-jest.mock('../logic/AI', () => ({
-  makeAIMove: jest.fn()
-}));
+import * as AI from '../logic/AI';
 
 describe('GameBoard component', () => {
   beforeEach(() => {
     // Clear all mocks before each test
     jest.clearAllMocks();
 
-    // Reset the mock to return true
-    jest.spyOn(gameHeuristics, 'checkForWin').mockReturnValue(true);
-
-    let board;
-    // Initialize board before each test
-    board = Array(20).fill(null).map(() => Array(20).fill(null));
+    // Mock selectCell to return a specific cell
+    jest.spyOn(AI, 'selectCell').mockImplementation((availableCells) => {
+      return availableCells[0];
+    });
   });
 
   afterEach(() => {
     // Restore the original implementation after each test
-    gameHeuristics.checkForWin.mockRestore();
+    AI.selectCell.mockRestore();
   });
 
   window.alert = jest.fn();
@@ -41,13 +36,26 @@ describe('GameBoard component', () => {
   });
 
   test('resets the board after a win', async () => {
+    jest.spyOn(gameHeuristics, 'checkForWin').mockReturnValue(true);
+
     render(<GameBoard />);
 
     fireEvent.click(screen.getByTestId('cell-0-0'));
 
-    // Use waitFor to handle asynchronous state updates and setTimeout
     await waitFor(() => {
       expect(screen.getByTestId('cell-0-0').textContent).toBe('');
+    });
+
+    gameHeuristics.checkForWin.mockRestore();
+  });
+
+  test('AI makes a move after the players turn', async () => {
+    render(<GameBoard /> );
+
+    fireEvent.click(screen.getByTestId('cell-0-1'));
+
+    await waitFor(() => {
+      expect(screen.getByTestId('cell-0-0').textContent).toBe('O');
     });
   });
 });
